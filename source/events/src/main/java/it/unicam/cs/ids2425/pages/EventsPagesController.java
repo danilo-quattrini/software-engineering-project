5f6769bca96f9436d5366526eed8f1252e3c41b1
PPathfinderIV <christiandevince@gmail.com>
2025-12-02 18:46:37 +0100

    [add] Aggiunto la possibilità di condivedere l'evento

--- source/events/src/main/java/it/unicam/cs/ids2425/pages/EventsPagesController.java
+++ source/events/src/main/java/it/unicam/cs/ids2425/pages/EventsPagesController.java
@@ -1,16 +1,20 @@
 package it.unicam.cs.ids2425.pages;
 
+import it.unicam.cs.ids2425.events.EventService;
 import it.unicam.cs.ids2425.events.EventState;
 import it.unicam.cs.ids2425.events.EventType;
 import it.unicam.cs.ids2425.users.repository.UserRepository;
 import it.unicam.cs.ids2425.users.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Controller("eventsPagesController")
@@ -18,17 +22,21 @@ import java.util.Optional;
 public class EventsPagesController {
 
     private final UserRepository userRepository;
+    private final EventService eventService;
 
-    public EventsPagesController(UserRepository userRepository) {
+    public EventsPagesController(UserRepository userRepository, EventService eventService) {
         this.userRepository = userRepository;
+        this.eventService = eventService;
     }
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("")
-    public String events(Model model, Authentication authentication) {
+    public String events(Model model, Authentication authentication, HttpServletRequest request) {
         populateCommonAttributes(model, authentication);
         model.addAttribute("eventTypes", EventType.values());
         model.addAttribute("eventStates", EventState.values());
+        String pageurl = request.getRequestURL().toString();
+        model.addAttribute("pageurl", pageurl);
         return "events";
     }
 
@@ -38,6 +46,18 @@ public class EventsPagesController {
         populateCommonAttributes(model, authentication);
         model.addAttribute("eventTypes", EventType.values());
         return "create-event";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/{eventId:\\d+}")
+    public String showEvent(@PathVariable("eventId") Long eventId,
+                            Model model,
+                            Authentication authentication,
+                            HttpServletRequest request) {
+        populateCommonAttributes(model, authentication);
+        model.addAttribute("event", eventService.getEvent(eventId));
+        model.addAttribute("pageUrl", request.getRequestURL().toString());
+        return "show";
     }
 
     private void populateCommonAttributes(Model model, Authentication authentication) {
